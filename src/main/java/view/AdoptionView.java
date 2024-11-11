92f13487f5c0d1a021d7cc0c2033207773fa7bcb
rachelkd <rachel05deng@gmail.com>
2024-11-10 23:51:21 -0500

    Fixing compilation errors related to adoption

--- src/main/java/view/AdoptionView.java
+++ src/main/java/view/AdoptionView.java
@@ -1,48 +1,45 @@
 package view;
 
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 
-import interface_adapter.adoption.AdoptionController;
-import interface_adapter.adoption.AdoptionState;
-import interface_adapter.adoption.AdoptionViewModel;
+// import interface_adapter.adoption.AdoptionController;
+// import interface_adapter.adoption.AdoptionState;
+// import interface_adapter.adoption.AdoptionViewModel;
 
 /**
- * The view for the Adopting a cat use case/
+ * The view for the Adopting a cat use case.
  */
 public class AdoptionView extends JPanel implements ActionListener, PropertyChangeListener {
-    private final AdoptionViewModel adoptionViewModel;
+    // private final AdoptionViewModel adoptionViewModel;
     private final JLabel name = new JLabel("Enter your new cat's name: ");
     private final JTextField nameField = new JTextField(15);
     private final String confirm = "Adopt now!";
     private final JButton confirmButton = new JButton(confirm);
 
-    public AdoptionView(AdoptionViewModel adoptionViewModel) {
-        this.adoptionViewModel = adoptionViewModel;
-        JPanel information = new JPanel();
-        information.add(name);
-        information.add(nameField);
-        JPanel finish = new JPanel();
-        finish.add(confirmButton);
+    // public AdoptionView(AdoptionViewModel adoptionViewModel) {
+    // this.adoptionViewModel = adoptionViewModel;
+    // JPanel information = new JPanel();
+    // information.add(name);
+    // information.add(nameField);
+    // final JPanel finish = new JPanel();
+    // finish.add(confirmButton);
 
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    // this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        this.add(information);
-        this.add(finish);
-    }
+    // this.add(information);
+    // this.add(finish);
+
+    // confirmButton.addActionListener(this);
+    // }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
