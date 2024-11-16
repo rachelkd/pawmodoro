c79d75d0ad65b4dd39754204f1485ace15a4f765
manahillsajid <manahill.sajid@mail.utoronto.ca>
2024-11-16 09:11:08 -0500

    Fixed checkstyle errors and tested the view.

--- src/main/java/interface_adapter/setupsession/SetupSessionViewModel.java
+++ src/main/java/interface_adapter/setupsession/SetupSessionViewModel.java
@@ -3,14 +3,14 @@ package interface_adapter.setupsession;
 import interface_adapter.ViewModel;
 
 /**
- * The view model for the setting up study session use case/
+ * The view model for the setting up study session use case.
  */
 public class SetupSessionViewModel extends ViewModel<SetupSessionState> {
     public static final String TITLE_LABEL = "Setup Study Session View";
     public static final String RETURN_BUTTON_LABEL = "Return";
-    public static final String STUDY_LABEL = "Study";
-    public static final String BREAK_LABEL  = "Break";
-    public static final String INSTRUCTIONS_LABEL = "Choose your study intervals: ";
+    public static final String STUDY_LABEL = "Study time: ";
+    public static final String BREAK_LABEL = "Break time: ";
+    public static final String INSTRUCTIONS_LABEL = "Choose your study intervals";
 
     public SetupSessionViewModel() {
         super("set up study session");
