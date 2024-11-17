9bdf33312addaf27ff4ca05b1240a7e276a1259b
rachelkd <rachel05deng@gmail.com>
2024-11-17 03:58:45 -0500

    Reverted last commit; removed user name to lowercase

--- src/main/java/entity/CommonUserFactory.java
+++ src/main/java/entity/CommonUserFactory.java
@@ -7,6 +7,6 @@ public class CommonUserFactory implements UserFactory {
 
     @Override
     public User create(String name, String password) {
-        return new CommonUser(name.toLowerCase(), password);
+        return new CommonUser(name, password);
     }
 }
