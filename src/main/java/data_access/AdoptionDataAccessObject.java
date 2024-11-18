a5c07efd80c30a761ca40ac146d884781b423501
manahillsajid <manahill.sajid@mail.utoronto.ca>
2024-11-18 12:53:17 -0500

    Fixed duplicate code - main should run now

--- src/main/java/data_access/AdoptionDataAccessObject.java
+++ src/main/java/data_access/AdoptionDataAccessObject.java
@@ -3,7 +3,15 @@ package data_access;
 import entity.Cat;
 import use_case.adoption.AdoptionDataAccessInterface;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AdoptionDataAccessObject implements AdoptionDataAccessInterface {
+    private final Map<Cat, String> catToUser = new HashMap<>();
+
+    public AdoptionDataAccessObject(Map<Cat, String> catToUser) {
+        this.catToUser.putAll(catToUser);
+    }
     @Override
     public boolean nameAlreadyExists(String catName) {
         return false;
