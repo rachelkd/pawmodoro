2d6d40020809ee352277b8a62f8d1c5d5158bfab
rachelkd <rachel05deng@gmail.com>
2024-11-18 20:13:13 -0500

    Removed unnecessary initialize call since that's already done in build method

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -14,7 +14,6 @@ public class Main {
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
         final JFrame application = appBuilder
-                .initialize()
                 .build();
 
         application.pack();
