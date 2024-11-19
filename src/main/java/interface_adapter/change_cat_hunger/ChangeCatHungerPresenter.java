6da8402c7a174682a650011c2b8ec0d509ff1b27
chiually <allyssa.chiu@mail.utoronto.ca>
2024-11-18 21:11:08 -0500

    small edit

--- src/main/java/interface_adapter/change_cat_hunger/ChangeCatHungerPresenter.java
+++ src/main/java/interface_adapter/change_cat_hunger/ChangeCatHungerPresenter.java
@@ -2,9 +2,7 @@ package interface_adapter.change_cat_hunger;
 
 import use_case.cat_management.change_cat_hunger.ChangeCatHungerOutputBoundary;
 import use_case.cat_management.change_cat_hunger.ChangeCatHungerOutputData;
-import view.BreakSession;
 import view.DisplayCatStatsView;
-import view.StudySessionView;
 
 public class ChangeCatHungerPresenter implements ChangeCatHungerOutputBoundary {
     private DisplayCatStatsView displayCatStatsView;
