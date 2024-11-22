557fac11fe18d62936b151e4702c7175d54d4342
manahillsajid <manahill.sajid@mail.utoronto.ca>
2024-11-21 23:58:55 -0500

    Adding test for setting up session

---
+++ src/test/java/use_case/setupsession/SetupSessionInputDataTest.java
@@ -0,0 +1,27 @@
+package use_case.setupsession;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetupSessionInputDataTest {
+    @Test
+    void successTest() {
+        final SetupSessionInputData inputData = new SetupSessionInputData(35, 10);
+        final SetupSessionOutputBoundary successPresenter = new SetupSessionOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SetupSessionOutputData outputData) {
+                assertEquals(35, outputData.getStudyTime());
+                assertEquals(10, outputData.getBreakTime());
+            }
+
+            @Override
+            public void switchToStudyView() {
+                //This is expected
+            }
+        };
+        final SetupSessionInputBoundary interactor = new SetupSessionInteractor(successPresenter);
+        interactor.execute(inputData);
+    }
+
+    // There is no fail test for this particular usecase.
+}
