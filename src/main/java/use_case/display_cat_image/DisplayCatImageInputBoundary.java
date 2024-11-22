81f756055ab6e2c7011eb1b093506ebcdadf87ad
rachelkd <rachel05deng@gmail.com>
2024-11-22 04:00:27 -0500

    Added more unit tests for 100% coverage; removed input data for display cat image use case

--- src/main/java/use_case/display_cat_image/DisplayCatImageInputBoundary.java
+++ src/main/java/use_case/display_cat_image/DisplayCatImageInputBoundary.java
@@ -6,8 +6,6 @@ package use_case.display_cat_image;
 public interface DisplayCatImageInputBoundary {
     /**
      * Executes the cat image use case.
-     * 
-     * @param inputData The input data for the use case
      */
-    void execute(DisplayCatImageInputData inputData);
+    void execute();
 } 
