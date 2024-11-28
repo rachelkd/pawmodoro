ac4b6ac2e5bc932b11ea19004e9ae7abebe714e0
Jinny Yoo <hyejin.yoo@mail.utoronto.ca>
2024-11-28 16:17:52 -0500

    deleted display timer use_cases.

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -177,11 +177,8 @@ public class LoginView extends JPanel implements ActionListener, PropertyChangeL
                     currentState.getUsername(),
                     currentState.getPassword());
             createInventoryController.execute(currentState.getUsername());
-            // TODO get rid of following code, currently for testing purposes
-            // addToInventoryController.execute(currentState.getUsername(), 0);
         }
         else if (evt.getSource().equals(backToSignUp)) {
-            // Switch to the Sign Up View
             loginController.switchToSignUpView();
         }
     }
